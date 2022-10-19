b37771bd79807c5de0c6a0c782a76d4246025a3a
Braco <isovicramiz07@gmail.com>
2022-10-18 20:29:16 -0400

    Refactored

--- index.js
+++ index.js
@@ -1,83 +1,83 @@
 
+const gameBoard = (() => {
+  let gameBoard = ['', '', '', '', '', '', '', '', ''];
+  
+  // cache dom
+  let ul = document.querySelector(".game-ul");
 
+  // render game board
+  function renderGameBoard() {
+    gameBoard.forEach(el => {
+      let li = document.createElement('li');
+      li.innerHTML += el;
+      ul.append(li);
+    })
+  };
 
-(function() {
+  function addId() {
+    let li = document.querySelectorAll('li');
+    let id = 0;
 
-    let gameBoard = {
-        gameBoard:  ['','', '',
-                     '','', '',
-                     '','', ''],
-        init: function() {
-            this.cacheDom();
-            this.renderGameBoard();
-            this.bindEvents();
-        },
-        cacheDom: function() {
-            this.ul = document.querySelector(".game-ul")
-        },
-        renderGameBoard: function() {
-            let board = {
-                gameBoard: this.gameBoard,
-            };
-            let id = 0;
-            board.gameBoard.forEach(el => { 
-                this.li = document.createElement("li");
-                this.li.setAttribute("id", id);
-                id++;
-                this.li.innerHTML += el;
-                this.ul.append(this.li);
-             });
-        },
-        bindEvents: function() {
-            this.ul.addEventListener("click", this.makeMove.bind(this));
-            this.ul.addEventListener("click", this.win.bind(this))
-        },
-        makeMove: function(e) {
-            let board = {
-                gameBoard: this.gameBoard,
-            }
+    li.forEach(el => {
+      el.setAttribute('id', id);
+      id++;
+    })
+  };
 
-            if (this.validMove(e)) {
-                let pos = e.target
-                let posId = e.target.id;
-                board.gameBoard[posId] = 'X'
-                pos.innerHTML = 'X';
-            } else {
-                throw 'ERROR'
-            }
-        },
-        validMove: function(e) {
-            if (e.target.innerHTML == '') {
-                return true;
-            } else {
-                return false;
-            }
-        },
-        win() {
-            let board = {
-                gameBoard: this.gameBoard
-            }
-            if (board.gameBoard[0] !== '' && board.gameBoard[1] !== '' && board.gameBoard[2] !== '' ||
-                board.gameBoard[3] !== '' && board.gameBoard[4] !== '' && board.gameBoard[5] !== '' ||
-                board.gameBoard[6] !== '' && board.gameBoard[7] !== '' && board.gameBoard[8] !== '' ||
-                board.gameBoard[0] !== '' && board.gameBoard[3] !== '' && board.gameBoard[6] !== '' ||
-                board.gameBoard[1] !== '' && board.gameBoard[4] !== '' && board.gameBoard[7] !== '' ||
-                board.gameBoard[2] !== '' && board.gameBoard[5] !== '' && board.gameBoard[8] !== '' ||
-                board.gameBoard[0] !== '' && board.gameBoard[4] !== '' && board.gameBoard[8] !== '' ||
-                board.gameBoard[6] !== '' && board.gameBoard[4] !== '' && board.gameBoard[2] !== '' ||
-                board.gameBoard[6] !== '' && board.gameBoard[4] !== '' && board.gameBoard[2] !== '' ) {
-                console.log("s")
-                return true;
-            }
-        },
-        test: function() {
-            let board = {
-                gameBoard: this.gameBoard,
-            }
-            if (board.gameBoard[0] !== '' && board.gameBoard[1] !== '' && board.gameBoard[2] !== '') {
-                return true;
-            }
-        }
+  renderGameBoard();
+  addId();
+
+  return {
+    gameBoard: gameBoard,
+    ul: ul
+  };
+})();
+
+const displayController = (() => {
+  const gb = gameBoard.gameBoard
+  let ul = gameBoard.ul
+  
+  ul.addEventListener("click", makeMove)
+  ul.addEventListener("click", win)
+
+  function makeMove(e) {
+    let pos = e.target;
+    let posId = e.target.id;
+    
+    if(validMove(e)) {
+    pos.innerHTML = "X";
+    gb[posId] = 'X'
+    console.log(e.target)
+    console.log(gb)
     }
-    gameBoard.init();
+  };
+
+  function validMove(e) {
+    if (e.target.innerHTML == '') {
+      return true;
+    } else {
+      throw "error"
+    }
+  }
+
+  function win() {
+    if (gb[0] !== "" && gb[1] !== "" && gb[2] !== "" ||
+        gb[0] !== "" && gb[3] !== "" && gb[6] !== "" ||
+        gb[0] !== "" && gb[4] !== "" && gb[8] !== "" ||
+
+        gb[1] !== "" && gb[4] !== "" && gb[7] !== "" ||
+
+        gb[2] !== "" && gb[5] !== "" && gb[8] !== "" ||
+        
+        gb[3] !== "" && gb[4] !== "" && gb[5] !== "" ||
+
+        gb[6] !== "" && gb[7] !== "" && gb[8] !== "" ||
+        gb[6] !== "" && gb[4] !== "" && gb[2] !== ""
+      ) {
+        console.log("win")
+        return true;
+      }
+  };
+
+
 })()
